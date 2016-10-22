1fe2bb75e325beeb9b279082a77b2857c5d74b21
Diego Marcher <marcher_diego@projectara.com>
2016-10-22 19:13:20 -0300

    Taller parte 2: uso del hardware

--- app/src/main/java/uy/com/nerdscorner/mobileday/CameraActivity.java
+++ app/src/main/java/uy/com/nerdscorner/mobileday/CameraActivity.java
@@ -1,13 +1,37 @@
 package uy.com.nerdscorner.mobileday;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CameraActivity extends AppCompatActivity {
+    private static final int IMAGE_FROM_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+    }
+
+    public void sacarFoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, IMAGE_FROM_CAMERA);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_FROM_CAMERA && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = ((ImageView) findViewById(R.id.image));
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
